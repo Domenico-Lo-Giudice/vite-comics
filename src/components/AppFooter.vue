fe8ed8957e091a98e39a1f9f3f2f885b9042a028
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-02-27 14:46:46 +0100

    Add components & base

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <div>FOOTER</div>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <div>HEADER</div>
+</template>
+
+<style scoped></style>
